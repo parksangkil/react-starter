45e2028c24d67e0558ab24d5d2469b857b21b89d
Honza Kalfus <jankalfus@gmail.com>
2016-08-01 17:25:08 +0200

    Updated TS build scripts - native + web

--- index.android.js
+++ index.android.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
-import App from './app/native';
+import App from './app/native/native';
 
 AppRegistry.registerComponent('reactWebNativeReduxStarter', () => App);
