5924d55927159619f63fa07c21dcfcd5661896a4
Honza Kalfus <jankalfus@gmail.com>
2016-08-01 15:03:38 +0200

    Working native build using TypeScript

--- index.android.js
+++ index.android.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
-import App from './app-prod';
+import App from './app-dev/native';
 
 AppRegistry.registerComponent('reactWebNativeReduxStarter', () => App);
