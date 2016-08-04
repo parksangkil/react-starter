2f74716c7f5098e373ec3944767486857ccbaa75
Honza Kalfus <jankalfus@gmail.com>
2016-08-04 12:12:58 +0200

    Separated build outputs for web and native. Added scripts to easily build web and native independently, based on a shared tsconfig.json file (tools/generate-native-ts-config.js creates tsconfig.native.json based on tsconfig.json and adds ignore of es6-shim).

---
+++ tools/generate-native-ts-config.js
@@ -0,0 +1,16 @@
+var fs = require("fs");
+var tsconfig = JSON.parse(fs.readFileSync("tsconfig.json", "utf8"));
+
+var exclude = [
+    "typings/globals/es6-shim",
+    "typings/index.d.ts",
+    "node_modules",
+    "app",
+    "app-native",
+    "tools"
+];
+
+var tsconfigNative = tsconfig;
+tsconfigNative["exclude"] = exclude;
+
+fs.writeFileSync("tsconfig.native.json", JSON.stringify(tsconfigNative));
