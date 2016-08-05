d584e9e317496e4647b3e881f47033361a99c821
Honza Kalfus <jankalfus@gmail.com>
2016-08-05 13:48:37 +0200

    Added support for redux dev tools in developer mode

--- src/common/setUpStore.ts
+++ src/common/setUpStore.ts
@@ -1,11 +1,36 @@
-import {createStore, applyMiddleware} from "redux";
+import {createStore, applyMiddleware, compose} from "redux";
 import * as thunk from "redux-thunk";
 import {rootReducer} from "./reducers/root";
+
+const middleware: Array<any> = [thunk.default];
+
+function setUpReduxDevTools() {
+    return typeof window === "object" &&
+        typeof (window as any).devToolsExtension !== "undefined" ?
+        (window as any).devToolsExtension() : (f: any) => f;
+}
+
+declare var process: {
+    env: {
+        NODE_ENV: string
+    }
+};
+
+function applyMiddlewareAndInDevModeUseDevTools(middleware: Array<any>) {
+    if (process.env.NODE_ENV === "production") {
+        return applyMiddleware(...middleware);
+    } else {
+        return compose(
+            applyMiddleware(...middleware),
+            setUpReduxDevTools()
+        );
+    }
+}
 
 export function setUpStore(initialState: any) {
     return createStore(
         rootReducer,
         initialState,
-        applyMiddleware(thunk.default)
+        applyMiddlewareAndInDevModeUseDevTools(middleware)
     );
 };
